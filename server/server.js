3ead9887e40c29a1f41fd45ee19aa769efe91204
Azamat <azamat@MacBook-Air-Azamat.local>
2026-03-19 11:02:01 +0500

    update: prepare for deploy (vercel + railway)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import chatRoutes from "./routes/chat.routes.js";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
 
-const PORT = 5000;
+
 
 app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT}`)
