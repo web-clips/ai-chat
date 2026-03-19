1e2e7ea134b803d917c1ebba3e99b88b0920512f
Azamat <azamat@MacBook-Air-Azamat.local>
2026-03-19 13:56:27 +0500

    Update CORS for Vercel frontend

--- client/src/components/ui/MessageList.jsx
+++ client/src/components/ui/MessageList.jsx
@@ -2,7 +2,7 @@ import ChatMessage from './ChatMessage'
 
 export default function MessageList({ messages, bottomRef }) {
   return (
-    <div className="flex flex-col gap-3 p-4 overflow-y-auto h-[250px] sm:h-[300px] md:h-[400px] mt-4">
+    <div className="flex flex-col gap-3 p-4 overflow-y-auto h-[100%] sm:h-[100%] md:h-[100%] mt-4">
       {messages.map((msg, i) => (
         <ChatMessage key={i} msg={msg} />
       ))}
